c7822225534205eeb2680f60e8775c74932a1371
Andrew Malvani <andrewrmalvani@gmail.com>
2024-05-26 15:27:38 -0700

    Updated Contact APIGW resource with ApiGWv2 and CORS config

--- terraform/contactLambda.tf
+++ terraform/contactLambda.tf
@@ -79,32 +79,33 @@ resource "aws_iam_role_policy_attachment" "lambda_sns_publish_attach" {
   policy_arn = aws_iam_policy.lambda_sns_publish.arn
 }
 
-resource "aws_api_gateway_rest_api" "api" {
-  name        = "FormSubmissionAPI"
-  description = "API for form submission"
+resource "aws_apigatewayv2_api" "api" {
+  name          = "FormSubmissionAPI"
+  protocol_type = "HTTP"
+  description   = "API for contact us form submission"
+  cors_configuration {
+    allow_origins = ["andrewmalvani.com"]
+    allow_methods = ["POST", "OPTIONS"]
+    allow_headers = ["*"]
+
+  }
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "{proxy+}"
+resource "aws_apigatewayv2_route" "route" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
 
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
+resource "aws_apigatewayv2_integration" "integration" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
 
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.form_submission.invoke_arn
+  connection_type        = "INTERNET"
+  description            = "Lambda integration"
+  integration_method     = "POST"
+  integration_uri        = aws_lambda_function.form_submission.invoke_arn
+  payload_format_version = "2.0"
 }
 
 resource "aws_lambda_permission" "apigw" {
@@ -113,5 +114,5 @@ resource "aws_lambda_permission" "apigw" {
   function_name = aws_lambda_function.form_submission.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+  source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
