f9b3b9d29de6f4f020e04621e41d86953195e174
Andrew Malvani <andrewrmalvani@gmail.com>
2024-06-04 21:13:28 -0700

    Changed HTTP API to REST API to allow for automatic CORS and healthcheck responses

--- terraform/contactLambda.tf
+++ terraform/contactLambda.tf
@@ -79,62 +79,122 @@ resource "aws_iam_role_policy_attachment" "lambda_sns_publish_attach" {
   policy_arn = aws_iam_policy.lambda_sns_publish.arn
 }
 
-resource "aws_apigatewayv2_api" "api" {
-  name          = "FormSubmissionAPI"
-  protocol_type = "HTTP"
-  description   = "API for contact us form submission"
-  cors_configuration {
-    allow_origins = ["https://andrewmalvani.com"]
-    allow_methods = ["POST", "OPTIONS"]
-    allow_headers = ["*"]
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "FormSubmissionAPI"
+  description = "API for contact us form submission"
+}
 
+resource "aws_api_gateway_resource" "contact" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "contact"
+}
+
+resource "aws_api_gateway_method" "post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.contact.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "post_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.contact.id
+  http_method = aws_api_gateway_method.post_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true,
   }
 }
 
-resource "aws_apigatewayv2_route" "route" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "POST /{proxy+}"
-  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.contact.id
+  http_method = aws_api_gateway_method.post_method.http_method
+
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.form_submission.invoke_arn
 }
 
-resource "aws_apigatewayv2_integration" "integration" {
-  api_id           = aws_apigatewayv2_api.api.id
-  integration_type = "AWS_PROXY"
+resource "aws_api_gateway_integration_response" "post_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.contact.id
+  http_method = aws_api_gateway_method.post_method.http_method
+  status_code = aws_api_gateway_method_response.post_200.status_code
 
-  connection_type        = "INTERNET"
-  description            = "Lambda integration"
-  integration_method     = "POST"
-  integration_uri        = aws_lambda_function.form_submission.invoke_arn
-  payload_format_version = "2.0"
-}
-
-resource "aws_apigatewayv2_route" "options_route" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "OPTIONS /{proxy+}"
-  target    = "integrations/${aws_apigatewayv2_integration.options_integration.id}"
-}
-
-resource "aws_apigatewayv2_integration" "options_integration" {
-  api_id           = aws_apigatewayv2_api.api.id
-  integration_type = "MOCK"
-
-  request_templates = {
-    "application/json" = jsonencode({
-      statusCode = 200
-      responseTemplates = {
-        "application/json" = <<EOF
-{
-  "statusCode": 200,
-  "headers": {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'",
   }
 }
-EOF
-      }
-    })
+
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.contact.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "http_200_options" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.contact.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true,
   }
+}
+
+resource "aws_api_gateway_integration_response" "http_200_options" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.contact.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = aws_api_gateway_method_response.http_200_options.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'",
+  }
+}
+
+resource "aws_api_gateway_resource" "healthcheck" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "healthcheck"
+}
+
+resource "aws_api_gateway_method" "healthcheck_get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.healthcheck.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "healthcheck_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.healthcheck.id
+  http_method = aws_api_gateway_method.healthcheck_get.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration" "healthcheck" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.healthcheck.id
+  http_method = aws_api_gateway_method.healthcheck_get.http_method
+
+  type                    = "MOCK"
+}
+
+resource "aws_api_gateway_integration_response" "healthcheck_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.healthcheck.id
+  http_method = aws_api_gateway_method.healthcheck_get.http_method
+  status_code = aws_api_gateway_method_response.healthcheck_200.status_code
 }
 
 resource "aws_lambda_permission" "apigw" {
@@ -143,5 +203,5 @@ resource "aws_lambda_permission" "apigw" {
   function_name = aws_lambda_function.form_submission.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
